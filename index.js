b33b943581cede230837952c7ad0aa1cfc069c7d
NavodContrath <navod.contrath@gmail.com>
2025-03-12 15:49:04 +0100

    add first route

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ Creiamo il progetto base con una rotta / che ritorna un testo semplice con scrit
 Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
 Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
 Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post. */
+const express = require('express')
+const app = express()
+const port = 3002
+
+app.listen(port, () => { })
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
